b0c13ab48a1761a5486cc6f71dc33c00a7a58f5c
Travis Elnicky <travis.elnicky@gmail.com>
2018-02-28 06:55:15 -0700

    Initial commit

---
+++ lib/x_files.ex
@@ -0,0 +1,18 @@
+defmodule XFiles do
+  @moduledoc """
+  Documentation for XFiles.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> XFiles.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/x_files_test.exs
@@ -0,0 +1,8 @@
+defmodule XFilesTest do
+  use ExUnit.Case
+  doctest XFiles
+
+  test "greets the world" do
+    assert XFiles.hello() == :world
+  end
+end
